7725ea10f242e23c65cbdce4647d27cfe7640d71
yong <yikityong@gmail.com>
2024-06-14 17:10:25 +0800

    update gitignore

--- fyp/obj/Debug/net8.0/fyp.AssemblyInfo.cs
+++ fyp/obj/Debug/net8.0/fyp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fyp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67587b6009f46e4d537e8c229cd59380af4273e")]
 [assembly: System.Reflection.AssemblyProductAttribute("fyp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fyp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
