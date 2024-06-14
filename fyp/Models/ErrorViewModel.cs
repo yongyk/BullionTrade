d1d7b9a37be8e92cd7f166a76679bfb0b4788685
yong <yikityong@gmail.com>
2024-06-14 16:01:33 +0800

    start proj

---
+++ fyp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace fyp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
