9ec30259890d0116ec73799d92da53d89e32b62a
yong <yikityong@gmail.com>
2024-06-15 13:31:02 +0800

    update

--- fyp/Program.cs
+++ fyp/Program.cs
@@ -25,6 +25,13 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+/*
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = $"/Identity/Account/Login";
+    options.LogoutPath = $"/Identity/Account/Logout";
+    options.AccessDeniedPath = $"/Identity/Account/AccessDenied";
+});*/
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
